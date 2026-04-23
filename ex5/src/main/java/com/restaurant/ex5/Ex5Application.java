375b418f0ee11197cd95f2c1f41cd57db0b4958b
DoTriCong <tricong5526@gmail.com>
2026-04-23 23:31:45 +0700

    Add files via upload

---
+++ ex5/src/main/java/com/restaurant/ex5/Ex5Application.java
@@ -0,0 +1,12 @@
+package com.restaurant.ex5;
+
+import com.restaurant.ex5.controller.MenuController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex5Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Ex5Application.class, args);
+    }
+}
